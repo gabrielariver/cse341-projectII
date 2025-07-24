ecc6bb741cc492f67bbf4f4060c49bc76ef368f1
gabrielariver <riv23011@byui.edu>
2025-07-24 17:19:16 -0500

    authors update with oauth

--- routes/authors.js
+++ routes/authors.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authorsController = require('../controllers/authors');
+const ensureAuth = require('../middleware/ensureAuth');
 
 router.get('/', authorsController.getAllAuthors);
 router.get('/:id', authorsController.getAuthorById);
-router.post('/', authorsController.createAuthor);
-router.put('/:id', authorsController.updateAuthor);
-router.delete('/:id', authorsController.deleteAuthor);
+router.post('/', ensureAuth, authorsController.createAuthor);
+router.put('/:id', ensureAuth, authorsController.updateAuthor);
+router.delete('/:id', ensureAuth, authorsController.deleteAuthor);
 
 module.exports = router;
