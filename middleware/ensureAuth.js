0d4eaab0e56cb3d78763018a5e90b4f778b54127
gabrielariver <riv23011@byui.edu>
2025-07-24 17:23:01 -0500

    Update of instructions

--- middleware/ensureAuth.js
+++ middleware/ensureAuth.js
@@ -2,7 +2,7 @@ function ensureAuth(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.status(401).json({ message: 'No autorizado. Por favor inicia sesión con GitHub.' });
+  res.status(401).json({ message: 'Unauthorized. Please sign in with GitHub.' });
 }
 
 module.exports = ensureAuth;
