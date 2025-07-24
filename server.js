bc4eb4d97f2aac30081ca45c86b21513474e47f6
gabrielariver <riv23011@byui.edu>
2025-07-24 16:35:21 -0500

    Update host to render

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const { initDb } = require('./data/books');
 const booksRoutes = require('./routes/books');
-const authorsRoutes = require('./routes/authors'); 
+const authorsRoutes = require('./routes/authors');
 const swaggerSetup = require('./swagger');
 require('dotenv').config();
 
-//passport sessions
+// session
 const session = require('express-session');
 const passport = require('passport');
 require('./services/passport');
-
 const authRoutes = require('./routes/auth');
 
-app.use('/auth', authRoutes);
+const app = express(); 
+const port = process.env.PORT || 3000; 
 
-const app = express();
-const port = process.env.PORT || 3000;
-
+// middlewares
 app.use(express.json());
 
-app.use(session({       
+app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
@@ -27,13 +25,17 @@ app.use(session({
     secure: process.env.NODE_ENV === 'production'
   }
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
+// routes
+app.use('/auth', authRoutes);
 swaggerSetup(app);
 app.use('/api/books', booksRoutes);
-app.use('/api/authors', authorsRoutes); 
+app.use('/api/authors', authorsRoutes);
 
+// mongodb conection
 initDb((err) => {
   if (err) console.error(err);
   else app.listen(port, () => console.log(`Server on http://localhost:${port}`));
--- services/passport.js
+++ services/passport.js
@@ -4,7 +4,7 @@ const GitHubStrategy = require('passport-github2').Strategy;
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: '/auth/github/callback'
+  callbackURL: 'https://cse341-projectii.onrender.com/auth/github/callback' 
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
